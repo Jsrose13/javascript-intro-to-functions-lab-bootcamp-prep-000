781d5a6bec9da8f12b072a9f420b9278afdc7ff3
JESSICA STEVENS <jsrose13@gmail.com>
2017-08-13 03:11:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string == string.toLowerCase()) 
               return ('I can\'t hear you!');
-  if (console.log('HELLO'))
-  return ('YES INDEED!')
-
+  if (string == string.toUpperCase()) 
+              return ('YES INDEED!');
+  if (string == string('I love you, Grandma.')) 
+              return ('I love you, too.');
 }
