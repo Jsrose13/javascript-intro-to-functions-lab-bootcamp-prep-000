fe2c2151c1922d6d003e4c2de7500fc435ee4593
JESSICA STEVENS <jsrose13@gmail.com>
2017-08-13 02:18:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout('shout') {
-  console.log('shout')
+function logShout(shout) {
+  console.log(shout)
   return string.toUpperCase()
 }
