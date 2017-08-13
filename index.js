291426b19a42c698f51bf700499f84258d48f597
JESSICA STEVENS <jsrose13@gmail.com>
2017-08-13 03:16:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function sayHiToGrandma(string) {
               return ('I can\'t hear you!');
   if (string == string.toUpperCase()) 
               return ('YES INDEED!');
-  if (string == string('I love you, Grandma.')) 
+  if (string == string.mixedCase('I love you, Grandma.')) 
               return ('I love you, too.');
 }
